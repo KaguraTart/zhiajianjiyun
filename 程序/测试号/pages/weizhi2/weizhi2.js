6b0aad8d575765e7e28dc7545873bca022b88348
Kagura Tart <diantaodeng@163.com>
2021-11-14 02:03:52 +0800

    123

---
+++ 程序/测试号/pages/weizhi2/weizhi2.js
@@ -0,0 +1,10 @@
+// pages/weizhi2/weizhi2.js
+let plugin = requirePlugin('routePlan');
+let key = 'FADBZ-QX7KG-P5QQZ-IPKV5-DWOMH-ZMFVR';  //使用在腾讯位置服务申请的key
+let referer = 'wxf8dc3e40a030fbc9';   //调用插件的app的名称
+let endPoint = JSON.stringify({  //终点
+    'name': '吉野家(北京西站北口店)',
+    'latitude': 39.89631551,
+    'longitude': 116.323459711
+});
+
